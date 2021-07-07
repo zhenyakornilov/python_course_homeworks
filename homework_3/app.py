0d8a3f02bd3cc4b527a347644e570eec3e707d7d
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-07-07 20:38:05 +0300

    Updated app.py file

--- homework_3/app.py
+++ homework_3/app.py
@@ -1,25 +1,27 @@
 import csv
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sales.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 with open('homework3sales.csv') as sales:
     csv_read = list(csv.reader(sales, delimiter=';'))
-    new_data = []
+    sales_data = []
     for row in csv_read[1:]:
-        value = (row[0], row[1], row[2], row[3])
-        new_data.append(value)
+        datetime_obj = datetime.strptime(row[0], '%m/%d/%Y %H:%M')
+        value = (datetime_obj, row[1], row[2], row[3])
+        sales_data.append(value)
 
 
 class Sales(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    transaction_date = db.Column(db.String, unique=False)
+    transaction_date = db.Column(db.DateTime, unique=False, nullable=False)
     product = db.Column(db.String(80), unique=False, nullable=False)
     price = db.Column(db.Integer, unique=False, nullable=False)
     payment_type = db.Column(db.String, unique=False, nullable=False)
@@ -28,9 +30,17 @@ class Sales(db.Model):
         return '<Sales %r>' % self.transaction_date
 
 
-db.create_all()
-for row in new_data:
-    sales = Sales(transaction_date=row[0], product=row[1],
-                  price=row[2], payment_type=row[3])
-    db.session.add(sales)
-    db.session.commit()
+# db.create_all()
+# for row in sales_data:
+#     sales = Sales(transaction_date=row[0], product=row[1],
+#                   price=row[2], payment_type=row[3])
+#     db.session.add(sales)
+#     db.session.commit()
+# expenses = Sales.query(func.sum(Sales.price)).group_by(Sales.transaction_date)
+# print(expenses)
+
+
+price_sum = db.session.query(db.func.sum(Sales.price)).\
+    group_by(db.func.strftime('%Y-%m-%d', Sales.transaction_date)).all()
+print(price_sum)
+print(len(price_sum))
